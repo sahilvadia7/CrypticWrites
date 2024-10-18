4f239b202735468a40b7889aff5b21b8fee48936
sahil vadia <83268961+sahilvadia7@users.noreply.github.com>
2024-10-18 23:57:12 +0530

    Delete backend directory

--- backend/src/main/java/com/crypticwrites/be/repo/loginRepo.java
+++
@@ -1,9 +0,0 @@
-package com.crypticwrites.be.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.crypticwrites.be.model.LoginUser;
-
-public interface loginRepo extends JpaRepository<LoginUser, Long>{
-    LoginUser findByEmail(String email);
-
-}
